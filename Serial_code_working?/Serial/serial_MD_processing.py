b528de6c701d9fc191172d8364505706ad61f200
Sbonsor <58981897+Sbonsor@users.noreply.github.com>
2021-03-08 12:54:46 +0000

    Update serial_MD_processing.py

--- Serial_code_working?/Serial/serial_MD_processing.py
+++ Serial_code_working?/Serial/serial_MD_processing.py
@@ -2,20 +2,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-def separations_histogram(time_step,separations):
-    hist,bins = np.histogram(separations[time_step,:])
-    return hist, bins
-
-def LJ_potential(epsilon,sigma,rc,delta,r,a,b,c,d):
-    if(r<rc):
-        potential = 4*epsilon*(np.power(sigma/r,12)-np.power(sigma/r,6))
-    elif(r<rc+sigma):
-        potential = a + b*r + c*r**2+ d*r**3
-    else: 
-        potential = 0
-        
-    return potential
-
 ## Importing data
 imported_data = pd.read_csv("molecular_data.csv",sep = ' ')
 parameters = pd.read_csv("parameters.csv", sep = ' ')
@@ -45,37 +31,6 @@ y = np.reshape(y,(nsteps,N))
 vx = np.reshape(vx,(nsteps,N))
 vy = np.reshape(vy,(nsteps,N))
 
-## Calculating the separations, kinetic energy, and potential energy of all particles at each time step.
-separations = np.zeros((nsteps, int(N*(N-1)/2)))
-kinetic_energy = np.zeros((nsteps,N))
-potential_energy = np.zeros((nsteps,N))
-
-total_kinetic_energy = np.zeros(nsteps)
-total_potential_energy = np.zeros(nsteps)
-total_energy = np.zeros(nsteps)
-
-#for step in range(nsteps):
-#    kinetic_energy[step,:] = 0.5 * (np.power(vx[step,:],2)+np.power(vy[step,:],2))
-#    for i in range(N):
-#        for j in range(i+1,N):
-#            separations[step,i] = np.power(np.power(x[step,i]-x[step,j],2)+np.power(y[step,i]-y[step,j],2),1/2)
-#            potential_energy[step,i] = potential_energy[step,j] = potential_energy[step,i] + 0.5*LJ_potential(epsilon,sigma,rc,delta,separations[step,i],a,b,c,d)
-#            
-#    total_kinetic_energy[step] = np.sum(kinetic_energy[step:])
-#    total_potential_energy[step] = np.sum(potential_energy[step:])
-#    total_energy[step] = total_kinetic_energy[step] + total_potential_energy[step]
-            
-
-            
-    
-# Total energy of the system          
-#fig,ax = plt.subplots(1,1)
-#
-#ax.plot(total_energy)
-#ax.set_xlabel('Time step')
-#ax.set_ylabel('Total Energy')
-#ax.set_title('Total energy')
-
 # Snapshot of particles at a given timestep            
 fig,ax = plt.subplots(1,1)
 
@@ -84,33 +39,7 @@ ax.set_xlabel('x')
 ax.set_ylabel('y')
 ax.set_title('Particle positions at timestep=' + str(nsteps-1))
 
-# Histogram of particle separations            
-#fig,ax = plt.subplots(1,1)
-#
-#ax.hist(separations[nsteps-1,:], bins = 'auto')
-#ax.set_xlabel('Separation')
-#ax.set_ylabel('Frequency')
-#ax.set_title('Separation histogram')
 
-if(N==2):
-## Plots for a single pair of particles.
-    fig,ax = plt.subplots(1,1)
-    
-    ax.plot(y[:,0], label = 'Particle 1')
-    ax.plot(y[:,1], label = 'Particle 2')
-    ax.set_xlabel('Time step')
-    ax.set_ylabel('y')
-    ax.set_title('y positions')
-    ax.legend()
-    
-    fig,ax = plt.subplots(1,1)
-    
-    ax.plot(x[:,0], label = 'Particle 1')
-    ax.plot(x[:,1], label = 'Particle 2')
-    ax.set_xlabel('Time step')
-    ax.set_ylabel('x')
-    ax.set_title('x positions')
-    ax.legend()
 
 
 
